ba21ee286fb633e20f35800d3d79e349513e7afe
Laurent Meyer <lmeyer@ea4d.com>
2021-12-05 14:33:46 +0100

    Reorganize

--- JSON/Sydney.10.4.2/PatchDelphiUnits.bat
+++ JSON/Sydney.10.4.2/PatchDelphiUnits.bat
@@ -1,5 +1,6 @@
-patch.exe -u REST.JsonReflect.pas REST.JsonReflect.pas.10.4.2.diff
-patch.exe -u System.JSON.pas System.JSON.pas.10.4.2.diff
+patch.exe -u REST.JsonReflect.pas REST.JsonReflect.pas.patch
+patch.exe -u REST.Json.pas REST.Json.pas.patch
+patch.exe -u System.JSON.pas System.JSON.pas.patch
 del /Q *.rej
 pause
 
