9c2bac08d3ea9b8c4f6f05850985ebfcd5e2a16d
Laurent Meyer <GIT@ea4d.com>
2021-12-13 18:40:05 +0100

    Json Files Patcher

---
+++ Demos/common/uPatcherChecker.pas
@@ -0,0 +1,29 @@
+unit uPatcherChecker;
+
+interface
+
+ procedure PatcherChecker;
+
+implementation
+uses REST.Json, Dialogs;
+
+type
+  TPatcherCheckedType = class
+    _DummyVariant: string;
+  end;
+
+procedure PatcherChecker;
+var
+  Checker: TPatcherCheckedType;
+begin
+  Checker := TPatcherCheckedType.Create;
+  var s := TJson.ObjectToJsonString(Checker);
+  if pos('_DummyVariant', s)>0  then  // Properties starting by '_' shouldn't be serialized...
+  begin
+    ShowMessage('JSON/REST Files are not patched !! Please use Patcher.exe or include the patched files (see Demo''s source project file)');
+    Halt;
+  end;
+  Checker.Free;
+end;
+
+end.
