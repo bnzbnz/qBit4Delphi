b094264fd0ca345877d51107f5310b2cf8efa9c3
Laurent Meyer <GIT@ea4d.com>
2021-12-13 19:21:38 +0100

    Exe signing

---
+++ signexe.bat
@@ -0,0 +1,6 @@
+signtool sign /a /fd sha256 /t http://time.certum.pl /v qNOXify.exe
+signtool sign /a /fd sha256 /t http://time.certum.pl /v FMXReport.exe
+signtool sign /a /fd sha256 /t http://time.certum.pl /v NOXMonitor.exe
+signtool sign /a /fd sha256 /t http://time.certum.pl /v Patcher.exe
+pause
+
