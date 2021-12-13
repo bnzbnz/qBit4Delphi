c26e27ccbf2cf919934db4566413a77447081cca
Laurent Meyer <GIT@ea4d.com>
2021-12-13 17:08:26 +0100

    Json Files Patcher

---
+++ JSON/22/PatchDelphiUnits.bat
@@ -0,0 +1,8 @@
+patch.exe -u REST.JsonReflect.pas REST.JsonReflect.pas.patch
+patch.exe -u REST.Json.pas REST.Json.pas.patch
+patch.exe -u System.JSON.pas System.JSON.pas.patch
+del /Q *.rej
+pause
+
+
+
