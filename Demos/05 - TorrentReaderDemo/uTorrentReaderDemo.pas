1b0a585ef3da0e1bc965c5c103a752cbd770fbf9
Laurent Meyer <lmeyer@ea4d.com>
2022-03-07 15:16:32 +0100

    Torrent Reader Typo

--- Demos/05 - TorrentReaderDemo/uTorrentReaderDemo.pas
+++ Demos/05 - TorrentReaderDemo/uTorrentReaderDemo.pas
@@ -5,8 +5,7 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, uTorrentReader,
-
-  uBEncode, DateUtils, System.Gnerics.Collections, System.Generics.Defaults,
+  uBEncode, DateUtils, System.Generics.Defaults, System.Generics.Collections,
   Vcl.ExtCtrls;
 
 type
