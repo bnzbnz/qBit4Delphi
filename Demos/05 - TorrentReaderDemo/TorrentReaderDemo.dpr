b50a1c897fd1c767cc10971c72cd096bf53f64e7
Laurent MEYER <32573002+bnzbnz@users.noreply.github.com>
2022-08-07 14:33:18 +0200

    Demos to Debug Configuration

--- Demos/05 - TorrentReaderDemo/TorrentReaderDemo.dpr
+++ Demos/05 - TorrentReaderDemo/TorrentReaderDemo.dpr
@@ -1,22 +1,16 @@
 program TorrentReaderDemo;
-
 uses
-
   {$IFDEF DEBUG}
     FastMM4,    //  MPL 1.1, LGPL 2.1 (https://github.com/pleriche/FastMM4)  << Can be removed if not used
   {$ENDIF}
-
   Vcl.Forms,
   uTorrentReaderDemo in 'uTorrentReaderDemo.pas' {Form2},
   uTorrentReader in '..\..\API\Tools\uTorrentReader.pas',
   uBEncode in '..\..\API\Tools\uBEncode.pas';
-
 {$R *.res} 
-
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TForm2, Form2);
   Application.Run;
 end.
-
